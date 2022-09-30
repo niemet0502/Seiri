743d3390808c65d1d17f1190ac0eaa02151029f9
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-09-30 15:43:41 +0000

    server: created the user entity

--- server/src/user/entities/user.entity.ts
+++ server/src/user/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  lastname: string;
+
+  @Column('text')
+  firstname: string;
+
+  @Column('text')
+  email: string;
+
+  @Column('text')
+  password: string;
+}
