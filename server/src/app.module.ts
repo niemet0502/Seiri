27b07adadf0d2971438d1b5e0a6bbb967f0f2ed3
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-09-30 13:47:59 +0000

    - installed typeOrm packages - setup the database connextion

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user/user.module';
+import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'thot',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly dataSource: DataSource) {}
+}
