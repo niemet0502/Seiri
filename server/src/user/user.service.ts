2ae2fb8b08954126699cc6ac7175a7d4d8f98708
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-09-30 15:44:59 +0000

    server: find all users features implementation

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all user`;
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
   }
 
   findOne(id: number) {
