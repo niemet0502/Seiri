367e45795f4e1194a6743538b66b776526cba9fe
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-10-02 15:42:38 +0000

    server: installed testing package

--- server/src/user/user.controller.spec.ts
+++ server/src/user/user.controller.spec.ts
@@ -3,18 +3,62 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userService: UserService;
+  let userController: UserController;
+
+  const mockService = {
+    findAll: jest.fn((dto) => {
+      return [
+        {
+          id: 1,
+          isConfirm: false,
+          lastname: 'Marius',
+          firstname: 'NIEMET',
+          email: 'mariusniemet@gmail.com',
+        },
+        {
+          id: 2,
+          isConfirm: false,
+          lastname: 'vincent',
+          firstname: 'NIEMET',
+          email: 'mariusniemet20@gmail.com',
+        },
+      ];
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [UserService],
-    }).compile();
+    })
+      .overrideProvider(UserService)
+      .useValue(mockService)
+      .compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const mockResponse = [
+        {
+          id: 1,
+          isConfirm: false,
+          lastname: 'Marius',
+          firstname: 'NIEMET',
+          email: 'mariusniemet@gmail.com',
+        },
+        {
+          id: 2,
+          isConfirm: false,
+          lastname: 'vincent',
+          firstname: 'NIEMET',
+          email: 'mariusniemet20@gmail.com',
+        },
+      ];
+
+      expect(await userController.findAll()).toEqual(mockResponse);
+    });
   });
 });
