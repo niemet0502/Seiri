215429ef582ece825124c9c0dbfc9c6e9d3de813
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-10-06 15:37:34 +0000

    server :: write unit test for the userRepository layer

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,7 +1,6 @@
 import {
   Body,
   Controller,
-  Delete,
   Get,
   HttpException,
   Param,
@@ -44,10 +43,5 @@ export class UserController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
   }
 }
