4d6ca44d487aba796e06cf492c8f725ddc963d17
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-11-10 17:38:58 +0000

    server :: generated task's resources

---
+++ server/src/task/task.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+@Injectable()
+export class TaskService {
+  create(createTaskDto: CreateTaskDto) {
+    return 'This action adds a new task';
+  }
+
+  findAll() {
+    return `This action returns all task`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} task`;
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto) {
+    return `This action updates a #${id} task`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} task`;
+  }
+}
