be292308e81028a41fa350594c7c5e3b14d5e626
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2022-11-13 10:50:15 +0000

    server :: created the task repository layer

---
+++ server/src/task/task.repository.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Project } from '../project/entities/project.entity';
+import { Task } from './entities/task.entity';
+
+@Injectable()
+export class TaskRepository {
+  constructor(
+    @InjectRepository(Task)
+    private tasksRepository: Repository<Task>,
+  ) {}
+
+  async save(task: Task) {
+    return await this.tasksRepository.save(task);
+  }
+
+  async findAll(): Promise<Task[]> {
+    return await this.tasksRepository.find();
+  }
+
+  async findAllByProject(project: Project): Promise<Task[]> {
+    return await this.tasksRepository.find({ where: { project: project } });
+  }
+
+  async findById(id: number): Promise<Task | undefined> {
+    return await this.tasksRepository.findOne({ where: { id: id } });
+  }
+
+  async remove(task: Task) {
+    return await this.tasksRepository.remove(task);
+  }
+}
