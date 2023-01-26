111cb0f2bf327e2ce709d0096b4c82f84732a186
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-01-26 23:09:27 +0000

    web :: updated the note card by displaying the data

--- web/src/components/NoteCard.tsx
+++ web/src/components/NoteCard.tsx
@@ -1,4 +1,30 @@
+import { AiOutlineDelete, AiOutlineStar } from "react-icons/ai";
+import { BsArchive } from "react-icons/bs";
+import { Note } from "../types";
+
 // will receive a note as arguments
-export const NoteCard: React.FC = () => {
-  return <div className="note-card">1</div>;
+export const NoteCard: React.FC<{ note: Note }> = ({ note }) => {
+  return (
+    <div className="note-card flex">
+      <div>
+        <h3>{note.title}</h3>
+
+        <p>{note.content}</p>
+      </div>
+
+      <div className="icons flex gap-2">
+        <div className="border-debug">
+          <AiOutlineDelete />
+        </div>
+
+        <div className="border-debug">
+          <BsArchive />
+        </div>
+
+        <div className="border-debug">
+          <AiOutlineStar />
+        </div>
+      </div>
+    </div>
+  );
 };
