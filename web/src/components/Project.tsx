c9ed37ab392da44d03c29bc7dab25a3bcb257e75
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-02-02 14:06:22 +0000

    web :: created a ProjectItem component

--- web/src/components/Project.tsx
+++ web/src/components/Project.tsx
@@ -1,3 +1,21 @@
-export const ProjectItem: React.FC<{}> = ({}) => {
-  return <div>Project</div>;
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { RxDotFilled } from "react-icons/rx";
+import { Project } from "../types";
+
+export const ProjectItem: React.FC<{ project: Project }> = ({ project }) => {
+  return (
+    <div
+      key={project.id}
+      className="project-items flex justify-content-between"
+    >
+      <div className="flex align-items-center project-title">
+        <RxDotFilled style={{ color: project.color }} />
+        <span style={{ fontSize: "14px" }}>{project.name}</span>
+      </div>
+      <div className="flex align-items-center">
+        <span className="notes-count">{project.notes.length}</span>
+        <BiDotsHorizontalRounded className="dot-icon" />
+      </div>
+    </div>
+  );
 };
