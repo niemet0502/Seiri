f3a41eb906f549546dd5abae63264fa089c67129
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-02-03 11:37:42 +0000

    web :: created a reusable Dropdown component

--- web/src/components/Dropdown.tsx
+++ web/src/components/Dropdown.tsx
@@ -1,3 +1,53 @@
-export const Dropdown: React.FC = () => {
-  return <div>Dropdown</div>;
+import { ReactElement, useEffect, useRef, useState } from "react";
+
+export const Dropdown: React.FC<{
+  trigger: (toggle: () => void, open: boolean) => ReactElement;
+  right?: string;
+  left?: string;
+  children: React.ReactNode;
+}> = ({ trigger, children, left = "-20px", right = "0px" }) => {
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const [isActive, setIsActive] = useState(false);
+  const onClick = () => setIsActive((prevVal) => !prevVal);
+
+  useEffect(() => {
+    const pageClickEvent = (e: MouseEvent) => {
+      // If the active element exists and is clicked outside of
+      if (
+        dropdownRef.current !== null &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
+        setIsActive(!isActive);
+      }
+    };
+
+    // If the dropdown is open then listen for clicks
+    // if (isActive) {
+    //   window.addEventListener("click", pageClickEvent);
+    // }
+
+    return () => {
+      window.removeEventListener("click", pageClickEvent);
+    };
+  }, [isActive]);
+
+  return (
+    <div style={{ position: "relative" }}>
+      {trigger(onClick, isActive)}
+      <div
+        className="dropdown flex flex-column"
+        ref={dropdownRef}
+        style={{
+          opacity: isActive ? 1 : 0,
+          visibility: isActive ? "visible" : "hidden",
+          transform: isActive ? "translateY(0)" : "translateY(-20px)",
+          right: right,
+          left: left,
+        }}
+        onClick={() => setIsActive(false)}
+      >
+        {children}
+      </div>
+    </div>
+  );
 };
