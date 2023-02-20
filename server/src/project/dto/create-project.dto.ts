00502f6c737cb18257d40e2a6c37928cbb8f31fa
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-02-20 09:13:53 +0000

    server :: generate documentation for the create project dto

--- server/src/project/dto/create-project.dto.ts
+++ server/src/project/dto/create-project.dto.ts
@@ -1,5 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateProjectDto {
+  @ApiProperty({ example: 'roadmap', description: "The project's  name" })
   public name: string;
+  @ApiProperty({
+    example: 'Chadwick',
+    description: "The project's  description",
+  })
   public description?: string;
+  @ApiProperty({ example: 'Chadwick', description: "The project's  userId" })
   public userId: number;
 }
