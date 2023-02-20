d8ca51933a725d2ffb5b67d673bdb82df4fc1912
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-02-20 09:13:42 +0000

    server :: generate documentation for the create note dto

--- server/src/note/dto/create-note.dto.ts
+++ server/src/note/dto/create-note.dto.ts
@@ -1,5 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateNoteDto {
+  @ApiProperty({
+    example: '2022 year in review',
+    description: "The note's title",
+  })
   public title: string;
+  @ApiProperty({ example: 'lorem ipsum', description: "The note's  content" })
   public content: string;
+  @ApiProperty({ example: '1', description: "The note's  projectId" })
   public projectId: number;
 }
