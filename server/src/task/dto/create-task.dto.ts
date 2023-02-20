2a11b9fcfd378f9a3beb0fb93affe4ed683cd796
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-02-20 09:14:04 +0000

    server :: generate documentation for the create task  dto

--- server/src/task/dto/create-task.dto.ts
+++ server/src/task/dto/create-task.dto.ts
@@ -1,5 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTaskDto {
+  @ApiProperty({
+    example: 'build a portfolio',
+    description: "The task's title",
+  })
   public title: string;
+  @ApiProperty({
+    example: 'create a new website ',
+    description: "The task's  description",
+  })
   public description: string;
+  @ApiProperty({ example: '1', description: "The task's  projectId" })
   public projectId: number;
 }
