3b201db059d6c01b57551d817f7fc2689c80534f
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-02-23 17:05:55 +0000

    server :: created a separate file for the typeorm data-source

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,28 +4,15 @@ import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { Session } from './auth/entities/session.entity';
-import { Note } from './note/entities/note.entity';
+import { dataSourceOptions } from './db/data-source';
 import { NoteModule } from './note/note.module';
-import { Project } from './project/entities/project.entity';
 import { ProjectModule } from './project/project.module';
-import { Task } from './task/entities/task.entity';
 import { TaskModule } from './task/task.module';
-import { User } from './user/entities/user.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USERNAME || 'root',
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME || 'thot',
-      entities: [User, Project, Session, Note, Task],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     UserModule,
     ProjectModule,
     TaskModule,
