a6c5c71e7d5374d43d9770cb05a1dfcaaa8a844d
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-02-24 17:46:42 +0000

    web :: token handling and Sign out feature

--- web/src/provider/userProvider.tsx
+++ web/src/provider/userProvider.tsx
@@ -1,11 +1,16 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
+import { history } from "../App";
 import { AuthRouting } from "../pages/Auth/AuthRouting";
 import { User } from "../types";
 
 interface CurrentContextValue {
   currentUser: User | null;
   setCurrentUser: (user: User) => void;
+  logout?: () => void;
 }
+
+export const TOKEN_LS_KEY = "userToken";
+export const USER_LS_KEY = "userInfo";
 
 export const CurrentUserContext = createContext<CurrentContextValue>(
   {} as CurrentContextValue
@@ -17,10 +22,36 @@ export const CurrentUserProvider: React.FC<{ children: any }> = ({
   const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   useEffect(() => {
-    console.log(currentUser);
+    try {
+      const token = localStorage.getItem(TOKEN_LS_KEY);
+
+      if (token) {
+        const user = JSON.parse(localStorage.getItem(USER_LS_KEY) as string);
+
+        setCurrentUser(user);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!currentUser) return;
+    localStorage.setItem(USER_LS_KEY, JSON.stringify(currentUser));
   }, [currentUser]);
+
+  const logout = useCallback(() => {
+    // query to the backend to destroy the token
+    localStorage.clear();
+    setCurrentUser(null);
+
+    history.push("/");
+  }, []);
+
   return (
-    <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
+    <CurrentUserContext.Provider
+      value={{ currentUser, setCurrentUser, logout }}
+    >
       {currentUser === null ? <AuthRouting /> : children}
     </CurrentUserContext.Provider>
   );
