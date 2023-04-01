9c1c7ff88b1a309028c747cb1fb1b25c37822083
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-04-01 03:13:36 +0000

    server :: created a new endpoint to delete multiple tasks from a given project

--- server/src/project/project.controller.ts
+++ server/src/project/project.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { User } from 'src/user/entities/user.entity';
@@ -72,5 +73,13 @@ export class ProjectController {
   @Delete(':id')
   async delete(@Param('id') id: string) {
     return await this.projectService.delete(+id);
+  }
+
+  @Delete(':id/tasks')
+  async deleteTasks(
+    @Param('id') id: string,
+    @Query('completed') completed: boolean,
+  ) {
+    return await this.projectService.deleteTask(+id, completed);
   }
 }
