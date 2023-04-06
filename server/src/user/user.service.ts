443b69293fad7330d36f44c53c5db01ffdcee6bf
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-04-06 19:23:50 +0000

    server :: replaced bcrypt by bcryptjs because i'm tired to fight with docker

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { LoginDto } from '../auth/dto/login.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
