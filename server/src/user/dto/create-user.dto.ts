c9a352cdec8e6b88c1d0226356e5d036f22dc1f6
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-05-04 23:38:44 +0000

    Update create-user.dto.ts

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -4,7 +4,7 @@ export class CreateUserDto {
   @ApiProperty({ example: 'Chadwick', description: "The user's firstname" })
   public firstname?: string;
 
-  @ApiProperty({ example: 'Boseman', description: "The user's first lastname" })
+  @ApiProperty({ example: 'Boseman', description: "The user's firstlastname" })
   public lastname?: string;
   @ApiProperty({
     example: 'chadwickboseman@gmail.com',
