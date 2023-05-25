2ba13482c362add042f47012fae066de7c5391cb
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-05-25 20:55:19 +0000

    server :: created workers that will pull job from the queue and execute them

--- server/src/mails/send-email-processor.service.ts
+++ server/src/mails/send-email-processor.service.ts
@@ -6,9 +6,41 @@ import { Logger } from '@nestjs/common';
 export class SendEmailProcessor {
   private readonly logger = new Logger('EmailSending');
   constructor(private readonly mailService: MailerService) {}
+
   @Process('welcomeEmail')
   async sendWelcomeEmail(job: any) {
     const { email } = job.data;
     this.logger.log(`job running ${email}`);
+
+    await this.mailService.sendMail({
+      to: email,
+      from: 'mariusvniemet@gmail.com',
+      subject: 'Welcome on Seiri',
+      template: 'welcome',
+      context: {
+        user: job.data,
+        web_app_link: job.data.web_app_link,
+        logo: process.env.WEB_APP_ADDRESS + 'white-logo.png',
+      },
+    });
+  }
+
+  @Process('resetPasswordEmail')
+  async resetPasswordEmail(job: any) {
+    const { email, token, user } = job.data;
+    this.logger.log(`job running ${email}`);
+
+    await this.mailService.sendMail({
+      to: email,
+      from: 'mariusvniemet@gmail.com',
+      subject: 'Seiri Password Reset Request',
+      template: 'resetpassword',
+      context: {
+        firstname: user.firstname,
+        lastname: user.lastname,
+        web_app_link: job.data.web_app_link,
+        logo: process.env.WEB_APP_ADDRESS + 'white-logo.png',
+      },
+    });
   }
 }
