085aca84ea118b13007994d8f22892866a5dc024
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-29 17:15:02 +0000

    website :: display a link on the header to the home page

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
 
@@ -5,13 +6,15 @@ const Header = ({ siteTitle }) => (
   <div className="header-container">
     <div className="header">
       <div className="header__child">
-        <StaticImage src="../images/white-logo.png" alt="logo" />
-        <h2>Seiri</h2>
+        <Link to="/">
+          <StaticImage src="../images/white-logo.png" alt="logo" />
+          <h2>Seiri</h2>
+        </Link>
       </div>
       <div className="header__child">
         <a>How it's built</a>
         <a>Changelog</a>
-        <a>Roadmap</a>
+        <Link to="/roadmap">Roadmap</Link>
       </div>
       <div className="header__child">
         <button className="btn secondary">Start for free</button>
