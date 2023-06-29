a3c3508ef8524b5e30bb25630ba12b1d47119a82
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-06-29 16:58:37 +0000

    website :: built the header

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -1,5 +1,23 @@
+import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
 
-const Header = ({ siteTitle }) => <header>Header</header>
+const Header = ({ siteTitle }) => (
+  <div className="header-container">
+    <div className="header">
+      <div className="header__child">
+        <StaticImage src="../images/white-logo.png" alt="logo" />
+        <h2>Seiri</h2>
+      </div>
+      <div className="header__child">
+        <a>How it's built</a>
+        <a>Changelog</a>
+        <a>Roadmap</a>
+      </div>
+      <div className="header__child">
+        <button className="btn secondary">Start for free</button>
+      </div>
+    </div>
+  </div>
+)
 
 export default Header
