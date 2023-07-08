23320f922c4ddc39582fb4a46bab693820daf034
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-07-08 08:43:15 +0000

    website :: displayed link on the footer

--- website/src/components/layout.js
+++ website/src/components/layout.js
@@ -27,7 +27,33 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div>
         <main>{children}</main>
-        <footer>Footer</footer>
+        <footer className="main-app-container">
+          <div className="main-app">
+            <p>
+              Built by{" "}
+              <a href="#!" target="_blank" rel="noreferrer">
+                @mariusniemet
+              </a>{" "}
+              UI inspirations by{" "}
+              <a
+                href="https://www.markdx.site/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                markdx.
+              </a>{" "}
+              The source code is available on{" "}
+              <a
+                href="https://github.com/niemet0502/Seiri"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+              .
+            </p>
+          </div>
+        </footer>
       </div>
     </>
   )
