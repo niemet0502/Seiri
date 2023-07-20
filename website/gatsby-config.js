03ece1e12400a13706624be55039be1ccca5d58f
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-07-20 21:02:01 +0000

    website :: removed unuse dependencies from the gastby config

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -48,8 +48,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
     /**
      * MARKDOWN
      */
